472032207515bb0882a4a6eb3494b524286e47e0
Abhishek Padalkar <30406681+Padlu@users.noreply.github.com>
2022-04-10 11:13:07 +0100

    Add files via upload

---
+++ Portfolio 1 (Data Exploration)/SQL_based_Data_Exploration.sql
@@ -0,0 +1,342 @@
+
+/*
+
+SQL based Covid Deaths and Vaccinations Data Exploration Project
+
+SQL Skills used: Creating Views, CTE's, Temp tables, Joins, Windows Functions, Aggregate Functions, Converting Data Types
+
+*/
+
+
+
+---------------------------------------------------------------------------------
+
+-- Checking the 2 tables [Covid Deaths and Covid Vaccinations]
+
+SELECT * 
+FROM SQL_SSMS_Portfolio_1.dbo.CovidDeaths
+order by 3,4
+
+SELECT * 
+FROM SQL_SSMS_Portfolio_1..CovidVaccinations
+order by 3,4
+
+
+--------------------------------------------------------------------------------- 
+
+/* Working on COVID DEATHS TABLE */
+
+-- (Based on TOTAL CASES) --
+
+
+
+-- For all the countries what is the death rate for total infected population
+
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+order by 1,2,3
+
+
+
+-- Likelihood of dying if you contract COVID in India (Checking the same for only country India)
+
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+WHERE location like '%ndia'
+order by 1,2,3
+
+
+
+-- What % of population got COVID in India (Total cases vs Population)
+
+SELECT location, date, total_cases, population, (total_cases/population)*100 as Case_Percentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+WHERE location like '%ndia'
+order by 1,2
+
+
+
+-- What countries have the highest infection rate compared to population
+
+SELECT location, population, MAX(total_cases) as Highest_Infection_count, MAX((total_cases/population))*100 as Percent_Population_infected
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Group by location, population   -- As we used an aggregate function
+order by Percent_Population_infected desc
+
+
+--------------------------------------------------------------------------------- 
+-- (Based on TOTAL DEATHS) --
+
+
+
+-- What countries have the highest death rate compared to population
+
+SELECT location, population, MAX(cast(total_deaths as int)) as Total_Death_count, MAX((cast(total_deaths as int)/population))*100 as Percent_Population_dead   -- Casting the variable into int for math since it is in nvarchar 
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null   -- This removes the rows with continental data to give accurate only country wise data
+Group by location, population
+order by Percent_Population_dead desc
+
+
+
+-- Same but comparing only for India, Ireland, United States and Norway
+
+SELECT location, population, MAX(cast(total_deaths as int)) as Total_Death_count, MAX((cast(total_deaths as int)/population))*100 as Percent_Population_dead
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where (location like '%states%' OR location like '%ndia' OR location like 'irel%' OR location like '%rway') AND (continent is not null)
+Group by location, population
+order by Percent_Population_dead desc
+
+
+
+-- Exploring death_counts by continent
+
+SELECT location, MAX(cast(total_deaths as int)) as Total_Death_count
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where (continent is null) AND (location not like '%income%')
+Group by location
+order by Total_Death_count desc
+
+
+--------------------------------------------------------------------------------- 
+
+-- (ENTIRE WORLD) ---- (TOTAL CASES) --
+
+
+
+-- Checking the data with Global numbers by removing location
+
+SELECT date, total_cases, total_deaths
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+order by 1
+
+
+
+/*
+
+For same date there's multiple columns as the data is created by location.
+Thus, here we explore it using aggregate function SUM()
+
+*/
+
+
+
+-- Using group by on date with aggregating functions.
+
+SELECT date, SUM(total_cases) as TotalCases, SUM(cast(total_deaths as int)) as TotalDeaths
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null
+Group by date
+order by 1
+
+/*
+
+The results are displayed such that every new date the total cases is sum of
+new_cases on that day and total cases of previous day.
+
+We double check that below by including new_cases just to be sure.
+
+*/
+
+
+SELECT date, SUM(total_cases) as TotalCases, SUM(new_cases) as NewCases, SUM(cast(total_deaths as int)) as TotalDeaths
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null
+Group by date
+order by 1 -- It's correct!
+
+
+
+-- (TOTAL DEATHS) --
+
+
+
+-- What is Global Death % per day
+
+SELECT date, SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, 
+SUM(cast(new_deaths as int))/SUM(new_cases)*100 as GlobalDeathPercentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null
+Group by date
+order by 1
+
+/*   There's 0.34% chance for a person to die across the world for date March 23rd, 2022.   */
+
+
+
+-- What is the global death percentage in this entire two year period.
+
+SELECT SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, 
+SUM(cast(new_deaths as int))/SUM(new_cases)*100 as GlobalDeathPercentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null
+order by 1 -- A little below 1.3%
+
+--------------------------------------------------------------------------------- 
+
+/* USING COVID VACCINATIONS TABLE WITH JOIN */
+
+
+-- Let's jump back to jog our memory what's on the vaccinations table.
+
+SELECT *
+FROM SQL_SSMS_Portfolio_1..CovidVaccinations
+order by 3,4
+
+
+
+-- Okay, now let's join the two tables on date, and location
+
+SELECT *
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+	JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.date = cv.date AND cd.location = cv.location
+--where cd.continent is not null
+order by cd.date, cd.location
+
+
+
+-- How many people in the world that have been vaccinated (Vaccinations vs population)
+
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.location = cv.location 
+		AND cd.date = cv.date
+WHERE cd.continent is not null
+order by 2,3
+
+
+-- Windows function to have a rolling count on total vaccinations per day per country
+
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
+	SUM(CONVERT(int, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, 
+	cd.date ROWS UNBOUNDED PRECEDING) as RollingTotalVaccinations -- Order by date as well to add the count per day wise and not entire location-- ROWS UNBOUNDED PRECEDING because of memory shortage due large data. 
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.location = cv.location 
+		AND cd.date = cv.date
+WHERE cd.continent is not null
+order by 2,3
+
+
+/* 
+
+ With the newly created variable RollingTotalVaccinations
+ we explore RollingTotalVaccinations % per population
+ To perform calculation on Partition By query we use CTE or Temp Table below
+
+*/
+
+
+
+-- Common Table Expression (CTE) --
+
+
+
+-- What is RollingTotalVaccinations % per population
+
+With PopvsVac (Continent, Location, Date, Population, NewVaccinations, RollingTotalVaccinations)
+as
+(
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
+	SUM(CONVERT(int, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, 
+	cd.date ROWS UNBOUNDED PRECEDING) as RollingTotalVaccinations
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.location = cv.location 
+		AND cd.date = cv.date
+WHERE cd.continent is not null
+)
+SELECT *, (RollingTotalVaccinations/Population)*100 as RollingVacPercentage
+FROM PopvsVac
+
+
+
+-- TEMP table --
+
+
+
+-- What is Max RollingTotalVaccinations % per population per location //Remove date
+
+DROP TABLE IF EXISTS #PercentPopulationVaccinated   -- drop if exists. Comes handy
+CREATE TABLE #PercentPopulationVaccinated 
+(
+Continent nvarchar(255), 
+Location nvarchar(255), 
+Population numeric, 
+NewVaccinations numeric, 
+RollingTotalVaccinations numeric
+)
+
+INSERT INTO #PercentPopulationVaccinated
+SELECT cd.continent, cd.location, cd.population, cv.new_vaccinations, 
+	SUM(CONVERT(bigint, cv.new_vaccinations)) OVER (PARTITION BY cd.location 
+	ORDER BY cd.location ROWS UNBOUNDED PRECEDING) as RollingTotalVaccinations
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.location = cv.location 
+		AND cd.date = cv.date
+WHERE cd.continent is not null
+
+SELECT Continent, Location, Population, MAX(NewVaccinations) as NewVac, MAX(RollingTotalVaccinations)
+	as RollingTotalVac, (MAX(RollingTotalVaccinations)/Population)*100 as RollingVacPercentage
+FROM #PercentPopulationVaccinated
+Group by Continent, Location, Population
+
+
+--------------------------------------------------------------------------------- 
+
+-- Creating Views for later usage or visualizations --
+
+
+
+-- View for Highest Infection rate per population
+
+CREATE VIEW HighestInfectionRateperPop 
+AS
+SELECT location, population, MAX(total_cases) as Highest_Infection_count, MAX((total_cases/population))*100 as Percent_Population_infected
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Group by location, population   -- As we used an aggregate function
+order by Percent_Population_infected desc
+
+
+
+-- View for Death counts per continent
+
+CREATE VIEW DeathCountsperContinent
+AS
+SELECT location, MAX(cast(total_deaths as int)) as Total_Death_count
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where (continent is null) AND (location not like '%income%')
+Group by location
+order by Total_Death_count desc
+
+
+
+-- View for Global Death % per day
+
+CREATE VIEW GlobalDeathRateperDay 
+AS
+SELECT date, SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, 
+SUM(cast(new_deaths as int))/SUM(new_cases)*100 as GlobalDeathPercentage
+FROM SQL_SSMS_Portfolio_1..CovidDeaths
+Where continent is not null
+Group by date
+order by 1
+
+
+
+-- View for Rolling Population Vaccinations
+
+CREATE VIEW RollingPeopleVaccinations 
+AS
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
+	SUM(CONVERT(int, cv.new_vaccinations)) OVER (PARTITION BY cd.location ORDER BY cd.location, 
+	cd.date ROWS UNBOUNDED PRECEDING) as RollingTotalVaccinations
+FROM SQL_SSMS_Portfolio_1..CovidDeaths cd
+JOIN SQL_SSMS_Portfolio_1..CovidVaccinations cv
+	ON cd.location = cv.location 
+		AND cd.date = cv.date
+WHERE cd.continent is not null
+
---
+++ Portfolio 2 (Data Cleaning)/SQL_based_Data_Cleaning.sql
@@ -0,0 +1,307 @@
+
+  /*
+
+  SQL based Data Cleaning for NashvilleHousing Data
+
+  SQL skills used: JOINS, CTE, ISNULL(), String Functions, conditional CASE statement, Converting data types, RENAME, UPDATE, ALTER TABLE, DROP
+
+  */
+
+  ------------------------------------------------------------------------------------------------
+
+  -- Standardize Date Format of SaleDate column
+  -- Remove unnecessary time part from it
+
+
+
+  SELECT SaleDate, CONVERT(date, SaleDate) as NewSaleDate
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- We Update the table with this new column
+
+  UPDATE NashvilleHousing
+  SET SaleDate = CONVERT(date, SaleDate)
+
+  SELECT SaleDate
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- Direct UPDATE not working so trying another method by using ALTER TABLE
+
+  ALTER TABLE NashvilleHousing
+  ADD NewSaleDate Date
+
+  UPDATE NashvilleHousing
+  SET NewSaleDate = CONVERT(date, SaleDate)
+
+  SELECT NewSaleDate
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+  ------------------------------------------------------------------------------------------------
+
+  -- Populate the NULL Property Address entries
+
+  SELECT PropertyAddress
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+  /* 
+  
+  In Real estate data, 99.99% chance is that property address cannot be NULL.
+  So we need to rectify it.
+
+  */
+
+
+  SELECT *
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+  /* 
+  
+  We see that there's a ParcelID column unique to each property address and also a UniqueID for each entry.
+ 
+  We can join the same table to itself on these two attributes such that uniqueID is not equal. 
+  
+  This will make sure that no same entries are joined and null addresses will be adjacent to real addresses for that Parcel ID
+
+  */
+
+
+
+  SELECT n1.[UniqueID ], n1.ParcelID, n1.PropertyAddress, n2.[UniqueID ], n2.ParcelID, n2.PropertyAddress
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing n1
+  JOIN SQL_SSMS_Portfolio_2..NashvilleHousing n2
+	ON n1.ParcelID = n2.ParcelID
+	AND n1.[UniqueID ] <> n2.[UniqueID ]
+  WHERE n1.PropertyAddress is null
+  ORDER BY n1.ParcelID
+
+
+
+  -- Populate this column with the one on the n2 side by using ISNULL Function.
+
+  SELECT n1.[UniqueID ], n1.ParcelID, n1.PropertyAddress, n2.[UniqueID ], n2.ParcelID, n2.PropertyAddress, 
+	ISNULL(n1.PropertyAddress, n2.PropertyAddress)
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing n1
+  JOIN SQL_SSMS_Portfolio_2..NashvilleHousing n2
+	ON n1.ParcelID = n2.ParcelID
+	AND n1.[UniqueID ] <> n2.[UniqueID ]
+  WHERE n1.PropertyAddress is null
+  ORDER BY n1.ParcelID
+
+
+
+  -- Make a final update in the table.
+
+  UPDATE n1
+  SET PropertyAddress = ISNULL(n1.PropertyAddress, n2.PropertyAddress)
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing n1
+  JOIN SQL_SSMS_Portfolio_2..NashvilleHousing n2
+	ON n1.ParcelID = n2.ParcelID
+	AND n1.[UniqueID ] <> n2.[UniqueID ]
+  WHERE n1.PropertyAddress is null
+
+  -- To check we run the previous query again and it has been updated as we wanted.  Nice Job!
+
+  ------------------------------------------------------------------------------------------------
+
+  -- Breaking out Address into individual columns to have more usability of the data (Address, City, State)
+
+
+
+  /* For Property Address */
+
+  SELECT PropertyAddress
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- For Street Address --
+
+  SELECT PropertyAddress, SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) as Address
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- For City --
+
+  SELECT PropertyAddress, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) as AddressCity
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- We UPDATE this in the real Table
+
+  ALTER TABLE NashvilleHousing
+  ADD Address nvarchar(255), AddressCity nvarchar(255)
+
+  UPDATE NashvilleHousing
+  SET Address = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1), 
+	AddressCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+
+  SELECT * 
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+	/*
+	-- Renaming the columns to PAddress and PAddressCity
+
+	EXEC sp_RENAME 'NashvilleHousing.Address', 'PAddress', 'COLUMN'
+	EXEC sp_RENAME 'NashvilleHousing.AddressCity', 'PAddressCity', 'COLUMN'
+	*/
+
+
+
+  /* Owner Address */
+
+  -- Performing same operation using PARSENAME()
+
+  SELECT OwnerAddress
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  SELECT PARSENAME(REPLACE(OwnerAddress,',','.'), 1)  -- Replacing ',' w/ '.' as Parsename looks for period as the delimiter
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  SELECT PARSENAME(REPLACE(OwnerAddress,',','.'), 3) as OAddress,  -- Street
+	  PARSENAME(REPLACE(OwnerAddress,',','.'), 2) as OAddressCity,   -- City
+	  PARSENAME(REPLACE(OwnerAddress,',','.'), 1) as OAddressState   -- State
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- We UPDATE the main Table
+
+  ALTER TABLE NashvilleHousing
+  ADD OAddress nvarchar(255), OAddressCity nvarchar(255), OAddressState nvarchar(255)
+
+  UPDATE NashvilleHousing
+  SET OAddress = PARSENAME(REPLACE(OwnerAddress,',','.'), 3), 
+	OAddressCity = PARSENAME(REPLACE(OwnerAddress,',','.'), 2),
+	OAddressState = PARSENAME(REPLACE(OwnerAddress,',','.'), 1)
+
+
+
+  SELECT * 
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+  -- Perfectly Done!
+
+
+  ------------------------------------------------------------------------------------------------
+  -- Standardizing values in "SoldAsVacant" Field ('y' -> 'Yes' and 'n' -> 'No')
+
+
+
+  SELECT DISTINCT SoldAsVacant
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- Understanding variable using DISTINCT and COUNT
+
+  SELECT DISTINCT SoldAsVacant, COUNT(SoldAsVacant) as Count_
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+  GROUP BY SoldAsVacant
+  ORDER BY 2
+
+/*
+
+  We see that only a few out of all the entries are unreliable. But, still we need to make this change for optimal use of the data.
+  For this we use CASE method When value is 'Y' Then put it as 'Yes' and When 'N' Then 'No'
+
+*/
+
+
+
+  SELECT SoldAsVacant,
+	CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	     WHEN SoldAsVacant = 'N' THEN 'No'
+		 ELSE SoldAsVacant
+		 END as NewSoldAsVacant
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- We UPDATE the main Table.
+
+  UPDATE NashvilleHousing
+  SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+						  WHEN SoldAsVacant = 'N' THEN 'No'
+						  ELSE SoldAsVacant
+						  END
+
+  ------------------------------------------------------------------------------------------------
+
+  -- Removing Duplicates (Not a good practice to delete data from the actual database. But, here I'm doing it just to showcase my knowledge)
+
+/*
+
+  This can be done using a Window function to first select the columns on which we base our duplicate rows and to give them a count
+  Then we use a CTE to delete the rows with count >1
+
+*/
+
+
+
+  SELECT *, (ROW_NUMBER() OVER
+	(PARTITION BY ParcelID,
+				PropertyAddress,
+				SalePrice,
+				SaleDate,
+				LegalReference
+				ORDER BY UniqueID)
+	) as RowNum
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+  -- Using CTE, WHERE and DELETE to remove duplicates
+
+  WITH DuplicateRowNum AS (
+  SELECT *, (ROW_NUMBER() OVER
+	(PARTITION BY ParcelID,
+				PropertyAddress,
+				SalePrice,
+				SaleDate,
+				LegalReference
+				ORDER BY UniqueID)
+	) as RowNum
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+  )
+  DELETE
+  FROM DuplicateRowNum
+  WHERE RowNum > 1
+
+
+  -- We checked by running the previous query that no duplicates are present now.
+  -- Nice Job Done!
+
+
+  ------------------------------------------------------------------------------------------------
+
+  -- Deleting Unused Columns
+
+  SELECT * 
+  FROM SQL_SSMS_Portfolio_2..NashvilleHousing
+
+
+
+  -- As we created new and usable addresses columns before, let's first get rid of the old address columns
+
+  ALTER TABLE NashvilleHousing
+  DROP COLUMN PropertyAddress, OwnerAddress, TaxDistrict
+
+
+
+  -- Let's also get rid of SaleDate
+
+  ALTER TABLE NashvilleHousing
+  DROP COLUMN SaleDate
